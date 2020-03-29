4517ae4003baecb8a243b335a8e19093bc893433
Andrew Cathcart <andrew.cathcart@salecycle.com>
2020-03-29 23:23:00 +0100

    super basic crud for tasks with status, description and title filtering

---
+++ src/tasks/task.model.ts
@@ -0,0 +1,12 @@
+export interface Task {
+  id: string;
+  title: string;
+  description: string;
+  status: TaskStatus;
+}
+
+export enum TaskStatus {
+  TO_DO = 'TO_DO',
+  IN_PROGRESS = 'IN_PROGRESS',
+  DONE = 'DONE',
+}
