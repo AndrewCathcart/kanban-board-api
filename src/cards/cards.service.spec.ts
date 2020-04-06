254822ce696270736414946b25718ffca70164e3
Andrew Cathcart <andrew.cathcart@salecycle.com>
2020-04-06 12:24:05 +0100

    cardsService unit tests complete

--- src/cards/cards.service.spec.ts
+++ src/cards/cards.service.spec.ts
@@ -31,6 +31,8 @@ describe('CardsService', () => {
       .useValue({
         getCards: jest.fn(),
         findOne: jest.fn(),
+        createCard: jest.fn(),
+        delete: jest.fn(),
       })
       .compile();
 
@@ -77,6 +79,72 @@ describe('CardsService', () => {
       expect(cardsService.getCardById(1, mockUser)).rejects.toThrow(
         new NotFoundException('Card with id "1" could not be found.'),
       );
+    });
+  });
+
+  describe('createCard', () => {
+    it('calls cardRepository.create() and returns the result', async () => {
+      (cardRepository.createCard as jest.Mock).mockResolvedValue(
+        'createCardResult',
+      );
+      expect(cardRepository.createCard).not.toHaveBeenCalled();
+      const createCardDto = {
+        title: 'Test card',
+        description: 'Test description',
+      };
+
+      const result = await cardsService.createCard(createCardDto, mockUser);
+
+      expect(cardRepository.createCard).toHaveBeenCalledWith(
+        createCardDto,
+        mockUser,
+      );
+      expect(result).toEqual('createCardResult');
+    });
+  });
+
+  describe('deleteCard', () => {
+    it('calls cardRepository.deleteCard() to delete a card', async () => {
+      (cardRepository.delete as jest.Mock).mockResolvedValue({ affected: 1 });
+      expect(cardRepository.delete).not.toHaveBeenCalled();
+
+      await cardsService.deleteCard(1, mockUser);
+
+      expect(cardRepository.delete).toHaveBeenCalledWith({
+        id: 1,
+        userId: mockUser.id,
+      });
+    });
+
+    it('throws an error as the card could not be found', () => {
+      (cardRepository.delete as jest.Mock).mockResolvedValue({ affected: 0 });
+
+      expect(cardsService.deleteCard(1, mockUser)).rejects.toThrow(
+        new NotFoundException(
+          'Card with id "1" could not be deleted as it does not exist.',
+        ),
+      );
+    });
+  });
+
+  describe('updateCardStatus', () => {
+    it('updates a card status', async () => {
+      const save = jest.fn().mockResolvedValue(true);
+      cardsService.getCardById = jest.fn().mockResolvedValue({
+        status: CardStatus.TO_DO,
+        save,
+      });
+      expect(cardsService.getCardById).not.toHaveBeenCalled();
+
+      const result = await cardsService.updateCardStatus(
+        1,
+        CardStatus.IN_PROGRESS,
+        mockUser,
+      );
+
+      expect(cardsService.getCardById).toHaveBeenCalled();
+      expect(save).toHaveBeenCalled();
+      expect(result.status).toEqual(CardStatus.IN_PROGRESS);
     });
   });
 });
